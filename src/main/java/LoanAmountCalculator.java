a8286acdd6422a856c6ee9f97adebca1204c4eaf
Jemma <jemmamillman@gmail.com>
2019-06-04 16:33:47 +0100

    added second method and new tests

--- src/main/java/LoanAmountCalculator.java
+++ src/main/java/LoanAmountCalculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class LoanAmountCalculator {
 
-    public String calculateLoanAmount(double monthlyRepayment, double interestRate, double yearsToBorrow) {
+    public String calculateAffordableLoanAmountUsingMonthlyPayment(double monthlyRepayment, double interestRate, int yearsToBorrow) {
         double maximumLoanAmountWithInterest = monthlyRepayment * yearsToBorrow * 12;
         double loanAmountWithInterest = 0;
         double outputLoanAmountWithInterest = 0;
@@ -19,26 +19,55 @@ public class LoanAmountCalculator {
             }
         }
         String loan = "The amount you will be able to borrow is £" + new DecimalFormat("##.##").format(outputLoan)
-                + ". The interest you will pay is £" + new DecimalFormat("##.##").format(outputInterest) + ". The total amount you will pay is £" + new DecimalFormat("##.##").format(outputLoanAmountWithInterest) + ".";
+                + ". The interest you will pay is £" + new DecimalFormat("##.##").format(outputInterest)
+                + ". The total amount you will pay is £" + new DecimalFormat("##.##").format(outputLoanAmountWithInterest)
+                + ".";
         System.out.println(loan);
         return loan;
     }
     public static void main(String[] args) {
         System.out.println("Loan Calculator!");
-        System.out.println("How much money can you afford to borrow? Let's work it out.");
+        System.out.println("Would you like to work out how much your loan will be based on affordability or a fixed loan amount?"
+                + "Please answer 1 for affordability or 2 for the total cost");
         Scanner input = new Scanner(System.in);
+        try {
+        int affordOrAmount = input.nextInt();
+
+        if (affordOrAmount==1){
+        System.out.println("How much money can you afford to borrow? Let's work it out.");
         System.out.println("");
         System.out.println("How much can you afford to repay each month?");
-        try {
+
             double monthlyRepayment = input.nextDouble();
             System.out.println("What's the interest rate percent?");
             double interestRate = input.nextDouble();
             System.out.println("How many years do you want to borrow for?");
-            double yearsToBorrow = input.nextDouble();
+            int yearsToBorrow = input.nextInt();
             LoanAmountCalculator loanAmountCalculator = new LoanAmountCalculator();
-            loanAmountCalculator.calculateLoanAmount(monthlyRepayment, interestRate, yearsToBorrow);
+            loanAmountCalculator.calculateAffordableLoanAmountUsingMonthlyPayment(monthlyRepayment, interestRate, yearsToBorrow);
+        }else{
+            System.out.println("How much money would you like to borrow? Let's work it out.");
+            System.out.println("");
+            System.out.println("How much would you like to borrow?");
+            double loanValue = input.nextDouble();
+            System.out.println("How many years do you want to borrow for?");
+            int yearsToBorrow = input.nextInt();
+            System.out.println("What's the interest rate percent?");
+            double interestRate = input.nextDouble();
+            LoanAmountCalculator loanAmountCalculator = new LoanAmountCalculator();
+            loanAmountCalculator.calculateTotalLoanAmountIncludingInterest(loanValue, interestRate, yearsToBorrow);
+        }
         } finally {
             input.close();
         }
+    }
+
+    public String calculateTotalLoanAmountIncludingInterest(double loanValue, double interestRate, int yearsToBorrow) {
+        double totalInterest = (interestRate/100*loanValue) * yearsToBorrow;
+        double totalLoanCost = totalInterest + loanValue;
+        System.out.println("To borrow £" + new DecimalFormat("##.##").format(loanValue) + ", over "  + yearsToBorrow
+                + " years at " + new DecimalFormat("##.##").format(interestRate) + "% APR, you will need to pay £"
+                + new DecimalFormat("##.##").format(totalLoanCost) + " in total.");
+        return "" + new DecimalFormat("##.##").format(totalLoanCost);
     }
 }
